b4dca99c55885867ce825ff6182d13413ef0d1c6
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-01 15:07:14 -0600

    Add key class

---
+++ lib/keys.rb
@@ -0,0 +1,20 @@
+class Keys
+  attr_reader :num, :random_numbers, :rand_keys
+
+  def initialize
+    @random_numbers = []
+    @rand_keys = {}
+  end
+
+  def random_number_generator
+    5.times{random_numbers << rand(10)}
+  end
+
+  def key_generator
+    @rand_keys["A"] = @random_numbers[0] + @random_numbers[1]
+    @rand_keys["B"] = @random_numbers[1] + @random_numbers[2]
+    @rand_keys["C"] = @random_numbers[2] + @random_numbers[3]
+    @rand_keys["D"] = @random_numbers[3] + @random_numbers[4]
+  end
+
+end
