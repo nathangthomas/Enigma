4956fffb0af6cd75dddb24a414ab8bcb37fc5dc5
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-02 09:51:29 -0600

    Add method for transforming message to ordinal values

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,7 +5,20 @@ class Enigma
     @message = message
   end
 
-  def make_message_lowercase
-    @message.downcase
+  # def make_message_lowercase
+  #   @message.downcase
+  # end
+
+  def transform_message_to_ordinal_values
+    array_form = @message.downcase.chars
+    ordinal_values = array_form.map do |letter|
+      letter.ord
+    end
+    ordinal_values
   end
+
+  # def encrypt(@message)
+  #   alphabet_size = 27
+  #   letters_to_nums = @message.chars.map(&:ord)
+  # end
 end
