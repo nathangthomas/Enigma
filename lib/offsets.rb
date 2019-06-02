e84c11192779929bfce2690055e3e4b134d515fe
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-01 18:34:26 -0600

    Add time.now

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -1,18 +1,22 @@
+require 'date'
 class Offsets
   attr_reader :date, :numbers, :offset_keys
 
   def initialize(date)
-    @date = date
+    @date = Time.now.strftime("%d%m%y")
     @numbers = last_4_digits_of_squared_date
     @offset_keys = {}
   end
 
-  def last_4_digits_of_squared_date
+  def date_squared
     square_of_date = @date.to_i ** 2
-    last_4 = square_of_date.to_s[-4..-1]
-    last_4.split(//).map{|num| num.to_i}
   end
 
+  def last_4_digits_of_squared_date
+    last_4 = date_squared.to_s[-4..-1]
+    last_4.split(//).map{|num| num.to_i}
+  end
+  
     def offset_generator
       @offset_keys["A"] = @numbers[0]
       @offset_keys["B"] = @numbers[1]
