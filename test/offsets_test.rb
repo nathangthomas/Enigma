42f80a6160391321a6367aba97206f4fb42535f1
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-03 11:10:17 -0600

    Add stub for date

--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -11,7 +11,7 @@ require 'pry'
 
 class OffsetsTest < MiniTest::Test
   def setup
-    @offsets = Offsets.new("060119")
+    @offsets = Offsets.new("010619")
   end
 
   def test_it_exists
@@ -19,11 +19,14 @@ class OffsetsTest < MiniTest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "010619", @offsets.date
+    @offsets.stub :date, "010619" do
+      assert_equal "010619", @offsets.date
+    end
   end
 
   def test_sqaure_date
     assert_equal 112763161, @offsets.date_squared
+
   end
 
   def test_returns_array_of_the_last_4_digits_of_squared_date
@@ -38,7 +41,7 @@ class OffsetsTest < MiniTest::Test
   end
 
   def test_the_offsets_returns_4_valid_offsets
-      @offsets.the_offsets
+    @offsets.offset_generator
 
     expected = {"A" => 3, "B" => 1, "C" => 6, "D" => 1}
     assert_equal expected, @offsets.offset_keys
