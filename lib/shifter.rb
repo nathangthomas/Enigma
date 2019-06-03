8866be85c15a43cf0074bd83637a886c5fa600b2
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-02 19:48:46 -0600

    Add working shift method

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -6,10 +6,25 @@ class Shifter
   end
 
   def shift_code
-    shift_code = @keys.merge(@offsets) do |key, rand_key, offset_key|
+   @keys.merge(@offsets) do |key, rand_key, offset_key|
       rand_key + offset_key
     end
-  shift_code
+
+  end
+
+  def shift(message)
+    message_to_array = message.downcase.chars
+    encrypted_message = ""
+    alphabet = ("a".."z").to_a << " "
+    interval = shift_code.keys.length
+
+    message_to_array.each_with_index do |letter, index|
+      keys_array = shift_code.keys
+      current_key = keys_array[index % interval]
+      shift_array = alphabet.rotate(shift_code[current_key])
+      encrypted_message << shift_array[alphabet.index(letter)]
+    end
+  encrypted_message
   end
 
 end
