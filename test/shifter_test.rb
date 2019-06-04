d1294116daa9b6e0b766e063f7ce3803ef569f3e
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-04 16:25:03 -0600

    Add test for keys with no parameters

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -28,13 +28,24 @@ class ShifterTest < MiniTest::Test
 
   def test_it_has_attributes
     assert @shifter.keys.length == 4
-    expected = {"A" => 3, "B" => 1, "C" => 6, "D" => 1}
+    expected = {"A" => 3, "B" => 1, "C" =>6, "D" =>1}
     assert_equal expected, @shifter.offsets
   end
 
   def test_shift_code
     assert_equal 4, @shifter.shift_code.length
-    expected = {"A"=>6, "B"=>6, "C"=>13, "D"=>10}
+    expected = {"A"=>15, "B"=>24, "C"=>40, "D"=>46}
     assert_equal expected, @shifter.shift_code
+  end
+
+  def test_returns_shifter_keys_with_no_parameters
+    #need to stub this test out. expected is different each time this is run.
+    @shifter_1 = Shifter.new(Keys.new.rand_keys, Offsets.new.offset_keys)
+
+    expected = {"A"=>73, "B"=>1, "C"=>7, "D"=>18}
+
+    assert_equal 4, @shifter_1.shift_code.keys.length
+    assert_equal 4, @shifter_1.shift_code.values.length
+    assert @shifter_1.shift_code
   end
 end
