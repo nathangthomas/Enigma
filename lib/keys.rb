75b09c2afaede66c3cefd41ef3e259bb158adfd3
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-04 14:15:28 -0600

    Adjust initialize

--- lib/keys.rb
+++ lib/keys.rb
@@ -2,8 +2,8 @@ class Keys
   attr_reader :num, :random_numbers, :rand_keys
 
   def initialize(random_numbers = random_number_generator)
-    @random_numbers = random_numbers.to_s.split(//).map {|char| char.to_i}
-    @rand_keys = {}
+    @random_numbers = random_numbers
+    @rand_keys = key_generator
   end
 
   def random_number_generator
@@ -13,10 +13,11 @@ class Keys
   end
 
   def key_generator
-    @rand_keys["A"] = @random_numbers[0] + @random_numbers[1]
-    @rand_keys["B"] = @random_numbers[1] + @random_numbers[2]
-    @rand_keys["C"] = @random_numbers[2] + @random_numbers[3]
-    @rand_keys["D"] = @random_numbers[3] + @random_numbers[4]
-    @rand_keys
+    rand_keys = {}
+    rand_keys["A"] = (@random_numbers[0] + @random_numbers[1]).to_i
+    rand_keys["B"] = (@random_numbers[1] + @random_numbers[2]).to_i
+    rand_keys["C"] = (@random_numbers[2] + @random_numbers[3]).to_i
+    rand_keys["D"] = (@random_numbers[3] + @random_numbers[4]).to_i
+    rand_keys
   end
 end
