6f14f82ebbaf0ff86035ca826dfdd0e12eb3a5f6
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-05 08:01:31 -0600

    Add if else to accomodate for special characters

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -20,10 +20,14 @@ class Enigma
     interval = shift.keys.length
 
     message_to_array.each_with_index do |letter, index|
-      keys_array = shift.keys
-      current_key = keys_array[index % interval]
-      shift_array = alphabet.rotate(shift[current_key])
-      encrypted_message << shift_array[alphabet.index(letter)]
+      if alphabet.include?(letter)
+        keys_array = shift.keys
+        current_key = keys_array[index % interval]
+        shift_array = alphabet.rotate(shift[current_key])
+        encrypted_message << shift_array[alphabet.index(letter)]
+      else
+        encrypted_message << letter
+      end
     end
     {encryption: encrypted_message, key: key, date: date}
   end
@@ -39,7 +43,9 @@ class Enigma
     interval = shift.keys.length
 
     reverse_shift = Hash.new{0}
-    shift.each{|key, value| reverse_shift[key] = value * -1}
+    shift.each do |key, value|
+      reverse_shift[key] = value * -1
+    end
 
     ciphertext_to_array.each_with_index do |letter, index|
       keys_array = reverse_shift.keys
