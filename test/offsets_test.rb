82ee0b5a7f4df2a4293bd1fb09a85d1e8070f888
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-05 00:23:21 -0600

    Add test

--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -16,6 +16,7 @@ class OffsetsTest < MiniTest::Test
   end
 
   def test_it_exists
+    Keys.any_instance.stubs(:random_numbers).returns("12345")
     assert_instance_of Offsets, @offsets_1
   end
 
