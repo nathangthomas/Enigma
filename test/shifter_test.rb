a471d3c3add589c0ad03cdee9c41db2b5b0c01cc
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-05 00:41:58 -0600

    Add test to test shifter with no parameters

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -15,37 +15,36 @@ class ShifterTest < MiniTest::Test
   def setup
     @keys = Keys.new("12345")
     @offsets = Offsets.new("010619")
-
     @keys.key_generator
     @offsets.offset_generator
-
-    @shifter = Shifter.new(@keys.rand_keys, @offsets.offset_keys)
+    @shifter_1 = Shifter.new(@keys.rand_keys, @offsets.offset_keys)
   end
 
   def test_it_exists
-    assert_instance_of Shifter, @shifter
+    assert_instance_of Shifter, @shifter_1
   end
 
   def test_it_has_attributes
-    assert @shifter.keys.length == 4
+    assert @shifter_1.keys.length == 4
     expected = {"A" => 3, "B" => 1, "C" =>6, "D" =>1}
-    assert_equal expected, @shifter.offsets
+    assert_equal expected, @shifter_1.offsets
   end
 
   def test_shift_code
-    assert_equal 4, @shifter.shift_code.length
+    assert_equal 4, @shifter_1.shift_code.length
     expected = {"A"=>15, "B"=>24, "C"=>40, "D"=>46}
-    assert_equal expected, @shifter.shift_code
+    assert_equal expected, @shifter_1.shift_code
   end
 
   def test_returns_shifter_keys_with_no_parameters
-    #need to stub this test out. expected is different each time this is run.
-    @shifter_1 = Shifter.new(Keys.new.rand_keys, Offsets.new.offset_keys)
+    Shifter.any_instance.stubs(:shift_code).returns({"A"=>15, "B"=>24, "C"=>40, "D"=>46})
+    @shifter_2 = Shifter.new(Keys.new.rand_keys, Offsets.new.offset_keys)
 
-    expected = {"A"=>73, "B"=>1, "C"=>7, "D"=>18}
+    expected = {"A"=>15, "B"=>24, "C"=>40, "D"=>46}
 
-    assert_equal 4, @shifter_1.shift_code.keys.length
-    assert_equal 4, @shifter_1.shift_code.values.length
-    assert @shifter_1.shift_code
+    assert_equal expected, @shifter_2.shift_code
+    assert_equal 4, @shifter_2.shift_code.keys.length
+    assert_equal 4, @shifter_2.shift_code.values.length
+    assert @shifter_2.shift_code
   end
 end
